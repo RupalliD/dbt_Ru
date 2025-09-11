959294d7d07703f17cd54532740da652536266d3
Rupalli devi <Rupallid@cloudeqs.com>
2025-09-11 07:45:05 +0000

    made new model

--- models/test_ru/stg_dept.sql
+++ models/test_ru/stg_dept.sql
@@ -1,5 +1,7 @@
 SELECT
-DEPARTMENT_ID ,
-DEPARTMENT_NAME,
-DEPARTMENT_HEAD
-FROM {{ source ('department_table','DEPARTMENTS')}}
+DEPARTMENT_ID AS dept_id,
+DEPARTMENT_NAME as department_name,
+UPPER(DEPARTMENT_HEAD) AS department_head,
+CURRENT_TIMESTAMP() AS dbt_loaded_at
+FROM {{ source('department_table','DEPARTMENTS')}}
+WHERE DEPARTMENT_ID IS NOT NULL 
